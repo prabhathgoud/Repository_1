1e84dcb5073d12bb3dff27ae4e9e855af2a9a890
prabhathgoud <67532311+prabhathgoud@users.noreply.github.com>
2020-06-28 11:07:50 +0530

    Update File_2.py

--- Directory_2/File_2.py
+++ Directory_2/File_2.py
@@ -1,17 +1,13 @@
-# Python3 program to find simple interest 
-# for given principal amount, time and 
-# rate of interest. 
+# Python3 program to find compound 
+# interest for given values. 
   
+def compound_interest(principle, rate, time): 
   
-def simple_interest(p,t,r): 
-    print('The principal is', p) 
-    print('The time period is', t) 
-    print('The rate of interest is',r) 
-      
-    si = (p * t * r)/100
-      
-    print('The Simple Interest is', si) 
-    return si 
-      
-# Driver code 
-simple_interest(8, 6, 8)
+    # Calculates compound interest  
+    CI = principle * (pow((1 + rate / 100), time)) 
+    print("Compound interest is", CI) 
+  
+# Driver Code  
+compound_interest(10000, 10.25, 5) 
+  
+# This code is contributed by Abhishek Agrawal.
